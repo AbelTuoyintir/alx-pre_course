129656d692e9016f7dc8df5c6d8319bc5ce41406
Abel <102219418+AbelTuoyintir@users.noreply.github.com>
2023-12-11 06:43:20 -0800

    ghana water company inventory management system

---
+++ GWCL/connect.php
@@ -0,0 +1,18 @@
+<?php
+    $assetbrand = $_POST['brand'];
+    $assetName = $_POST['asset-name'];
+    $assetType = $_POST['asset-type'];
+    $subAssetType = $_POST['sub-asset-type'];
+    $serialNumber = $_POST['serial-number'];
+    $assetTag = $_POST['asset-tag'];
+    $purschaseDate = $_POST['purchase-date'];
+    $purchaseAmount= $_POST['purchase-amount'];
+    $model = $_POST['model'];
+    $status = $_POST['status'];
+    $location = $_POST['location'];
+    $districts = $_POST['sub-location-type']
+    $assignedTo = $_POST['assignedTo'];
+    $office = $_POST['sub-office-type'],
+    $usageDsecription = $_POST['usage-description'];
+    $Purpose = $_POST['purpose'];
+?>
